f46550713a6fdee38dce16bc9a838e4e593666a4
arpit gupta <arpitgupta14786@gmail.com>
2025-07-27 02:06:45 +0530

    touched

---
+++ schema.js
@@ -0,0 +1,40 @@
+// const Joi =  require('joi');
+// module.exports.listingSchema = Joi.object({
+//     listing: Joi.object({
+//         title: Joi.string().required(),
+//         description: Joi.string().required(),
+//         location: Joi.string().required(),
+//         country: Joi.string().required(),
+//         price: Joi.number().required().min(0), // price ko number hona chahiye aur 0 se kam nahi
+//         image: Joi.string().allow("", null) // image ko string ya null ya empty string allow karega
+//     }).required()
+// });
+
+// module.exports.reviewSchema = Joi.object({
+//     review: Joi.object({
+//         rating: Joi.number().required().min(1).max(5), 
+//         comment: Joi.string().required(),
+//     }).required(),
+// });
+
+const Joi = require('joi');
+
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.object({
+            url: Joi.string().uri().required()
+        }).required()
+    }).required()
+});
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required(),
+    }).required(),
+});

7eea5021e305ddb293c3e85f89cb74ea51b025af
arpit gupta <arpitgupta14786@gmail.com>
2025-07-28 18:09:11 +0530

    modified routes for authentication & authorization

--- routes/review.js
+++ routes/review.js
@@ -2,41 +2,32 @@ const express = require("express");
 const router = express.Router({mergeParams: true}); // mergeParams ko true karne se ye listing ke id ko bhi access kar sakta h
 const wrapAsync = require("../utils/wrapAsync.js"); 
 const ExpressError= require("../utils/ExpressError.js"); 
-const {reviewSchema} = require("../schema.js"); 
 const Review = require("../models/review.js"); 
 const Listing = require("../models/listing.js");
+const { validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
-const validateReview = (req, res, next) => {
-    let {error} = reviewSchema.validate(req.body);
-    if(error){ // agar error aata h to ye error handle karega
-        let errMsg = error.details.map((el) => el.message).join(", ");
-        throw new ExpressError(400, errMsg);
-    } else{
-        next(); // agar error nahi aata h to ye next middleware ko call karega
-    }
-  };
 
 //Post route for reviews
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-
+    newReview.author = req.user._id; 
     listing.reviews.push(newReview); // ye listing ke andar review ko push karega
 
     await newReview.save(); // ye review ko save karega
     await listing.save(); // ye listing ko save karega
-
+    req.flash("success", "New Review Created!");
     res.redirect(`/listings/${listing._id}`); 
 }));
     
 //Delete route for reviews
-router.delete("/:reviewId", wrapAsync(async (req, res) => {
+router.delete("/:reviewId",isLoggedIn, isReviewAuthor, wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
 
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }); // ye listing se review ko remove karega jo reviewId se match karega
     await Review.findById(reviewId);
-
+    req.flash("success", "Review Deleted!");
     res.redirect(`/listings/${id}`); 
 })
 );

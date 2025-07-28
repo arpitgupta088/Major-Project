dfb38c46f5422fe9e13592e12fd03bfbabb03918
arpit gupta <arpitgupta14786@gmail.com>
2025-07-28 18:08:12 +0530

    modified routes for authentication

--- routes/listing.js
+++ routes/listing.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");   //.. mtlb ham parent directory me ja rhe h
-const ExpressError = require("../utils/ExpressError.js");
-const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing} = require("../middleware.js"); 
 
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) { // agar error aata h to ye error handle karega
-        let errMsg = error.details.map((el) => el.message).join(", ");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next(); // agar error nahi aata h to ye next middleware ko call karega
-    }
-};
 
 
 //index route
@@ -23,14 +13,25 @@ router.get("/", wrapAsync(async (req, res) => {
 }));
 
 //new route
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("listings/new"); // ye new listing ka form render karega
 });
 
 //show route
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews"); // ye listing ko id se dhoondta h aur reviews ko populate karta h
+    const listing = await Listing.findById(id)
+    .populate({path:"reviews", 
+             populate: {
+                path: "author",
+             },
+    })
+    .populate("owner"); // ye listing ko id se dhoondta h aur reviews ko populate karta h
+    if(!listing){
+        req.flash("error", "Listing you requested does not exist");
+        res.redirect("/listings");
+    }
+
     res.render("listings/show", { listing }); // ye listing ko show karega
 }));
 
@@ -39,16 +40,23 @@ router.get("/:id", wrapAsync(async (req, res) => {
 router.post("/", validateListing, wrapAsync(async (req, res, next) => {
 
     // let {title, description, image, price, location, country} = req.body; //iski jagah niche wali line likh skte h
-    const newlisting = new Listing(req.body.listing);
-    await newlisting.save();
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    await newListing.save();
+    req.flash("success", "New Listing Created"); // success is key and new listing created is message
     res.redirect("/listings"); // ye redirect karega listings ke index page par
 })
 );
 
 // edit route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+      if(!listing){
+        req.flash("error", "Listing you requested does not exist");
+        res.redirect("/listings");
+    }
+
     res.render("listings/edit", { listing });
 }));
 
@@ -58,10 +66,10 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 //     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 //     res.redirect(`/listings/${id}`); // ye redirect karega listing ke show page par
 // }));
-router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-
+    
     listing.title = req.body.listing.title;
     listing.description = req.body.listing.description;
     listing.price = req.body.listing.price;
@@ -71,15 +79,17 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
     listing.image.url = req.body.listing.image.url;
 
     await listing.save();
+    req.flash("success", "Listing Updated!"); 
     res.redirect(`/listings/${id}`);
 }));
 
 
 // delete route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id); // ye listing ko delete karega
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted Successfully!"); // ye 
     res.redirect("/listings");
 }));
 

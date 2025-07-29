395ad312619e3b985ca59ab3e5a451e30c9e4b30
arpit gupta <arpitgupta14786@gmail.com>
2025-07-29 17:50:33 +0530

    middlewares for authentication and authorization

---
+++ middleware.js
@@ -0,0 +1,61 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+
+module.exports.isLoggedIn = (req, res, next)=> {
+     if(!req.isAuthenticated()){
+        req.session.redirectUrl=req.originalUrl;
+        req.flash("error", "You must be logged in to create a listing");
+        return res.redirect("/login");
+    }
+    next(); // agar user logged in hai to ye next middleware ko call karega
+}
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if(req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl; 
+    }
+    next(); 
+};
+
+module.exports.isOwner = async (req, res, next) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if(!listing.owner._id.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the owner of this listing");
+        return res.redirect(`/listings/${id}`);
+    }
+    next(); // agar user owner hai to ye next middleware ko call karega
+};
+
+
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) { // agar error aata h to ye error handle karega
+        let errMsg = error.details.map((el) => el.message).join(", ");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next(); // agar error nahi aata h to ye next middleware ko call karega
+    }
+};
+
+module.exports.validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+    if(error){ // agar error aata h to ye error handle karega
+        let errMsg = error.details.map((el) => el.message).join(", ");
+        throw new ExpressError(400, errMsg);
+    } else{
+        next(); // agar error nahi aata h to ye next middleware ko call karega
+    }
+  };
+
+  module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author._id.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the author of this listing");
+        return res.redirect(`/listings/${id}`);
+    }
+    next(); // agar user owner hai to ye next middleware ko call karega
+};

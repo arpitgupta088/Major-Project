c5db2a2cdc49d3caa333d4d0f56138937f4809f4
arpit gupta <arpitgupta14786@gmail.com>
2025-07-29 17:58:50 +0530

    simplified using contrioller file

--- routes/listing.js
+++ routes/listing.js
@@ -4,61 +4,27 @@ const wrapAsync = require("../utils/wrapAsync.js");   //.. mtlb ham parent direc
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing} = require("../middleware.js"); 
 
-
+const listingController = require("../controllers/listings.js");
 
 //index route
-router.get("/", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index", { allListings });
-}));
+router.get("/", wrapAsync(listingController.index));    // jo kaam ho rha h wo controllers folder me dal diya h
+                                    //index var me aur use yaha require krliya
 
 //new route
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("listings/new"); // ye new listing ka form render karega
-});
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 //show route
-router.get("/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id)
-    .populate({path:"reviews", 
-             populate: {
-                path: "author",
-             },
-    })
-    .populate("owner"); // ye listing ko id se dhoondta h aur reviews ko populate karta h
-    if(!listing){
-        req.flash("error", "Listing you requested does not exist");
-        res.redirect("/listings");
-    }
-
-    res.render("listings/show", { listing }); // ye listing ko show karega
-}));
+router.get("/:id", wrapAsync(listingController.showListing));
 
 //create route
 
-router.post("/", validateListing, wrapAsync(async (req, res, next) => {
-
-    // let {title, description, image, price, location, country} = req.body; //iski jagah niche wali line likh skte h
-    const newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    await newListing.save();
-    req.flash("success", "New Listing Created"); // success is key and new listing created is message
-    res.redirect("/listings"); // ye redirect karega listings ke index page par
-})
+router.post("/", validateListing, 
+    wrapAsync(listingController.createListing)
 );
 
 // edit route
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-      if(!listing){
-        req.flash("error", "Listing you requested does not exist");
-        res.redirect("/listings");
-    }
-
-    res.render("listings/edit", { listing });
-}));
+router.get("/:id/edit", isLoggedIn, isOwner, 
+    wrapAsync(listingController.renderEditForm));
 
 //Update route
 // router.put("/:id", validateListing, wrapAsync(async (req, res) => {
@@ -66,31 +32,11 @@ router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
 //     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 //     res.redirect(`/listings/${id}`); // ye redirect karega listing ke show page par
 // }));
-router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    
-    listing.title = req.body.listing.title;
-    listing.description = req.body.listing.description;
-    listing.price = req.body.listing.price;
-    listing.location = req.body.listing.location;
-    listing.country = req.body.listing.country;
-
-    listing.image.url = req.body.listing.image.url;
-
-    await listing.save();
-    req.flash("success", "Listing Updated!"); 
-    res.redirect(`/listings/${id}`);
-}));
+router.put("/:id", isLoggedIn, isOwner, validateListing, 
+    wrapAsync(listingController.updateListing));
 
 
 // delete route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id); // ye listing ko delete karega
-    console.log(deletedListing);
-    req.flash("success", "Listing Deleted Successfully!"); // ye 
-    res.redirect("/listings");
-}));
+router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 module.exports = router; 
